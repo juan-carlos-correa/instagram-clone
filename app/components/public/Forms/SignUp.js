362ef60b69f64dfa5ac0fac8a6e501522b2414c8
juan-carlos-correa <juan.correa.herrera@gmail.com>
2018-06-02 15:25:34 -0500

    Add more fields to SignUpForm with new props

--- app/components/public/Forms/SignUp.js
+++ app/components/public/Forms/SignUp.js
@@ -2,19 +2,24 @@ import React from 'react'
 import { View, Text, StyleSheet, TextInput, Button }  from 'react-native'
 import { Field, reduxForm } from 'redux-form'
 
-const MyField = (props) => (
+const MyField = props => (
   <TextInput
-    placeholder="Username"
+    placeholder={props.placeholder}
     onChangeText={props.input.onChange}
     value={props.input.value}
+    keyboardType={props.keyboardType || 'default'}
+    autoCapitalize={props.autoCapitalize || 'none'}
+    secureTextEntry={props.secureTextEntry || false}
   />
 )
 
 const SignUpForm = props => {
   return (
     <View>
-      <Field name="username" component={MyField} />
-      <Field name="email" component={MyField} />
+      <Field name="username" component={MyField} placeholder="Username"/>
+      <Field keyboardType='email-address' name="email" component={MyField} placeholder="Email"/>
+      <Field secureTextEntry={true} name="password" component={MyField} placeholder="Password"/>
+      <Field secureTextEntry={true} name="repeatPassword" component={MyField} placeholder="Repeat password"/>
       <Button title="SignUp" onPress={props.handleSubmit(values => console.log(values))} />
     </View>
   )
